40cec6061214bec81486957742cdf28f834bf2cb
Liliane Castro <lcs2@icomp.ufam.edu.br>
2020-07-09 00:25:37 -0400

    feat(Enemy): Amangown appears to the nation's sorrow

--- Assets/Scripts/Other/Enemy.cs
+++ Assets/Scripts/Other/Enemy.cs
@@ -2,8 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyType{
+    NoAttack, AmangownAttack
+}
+
 public class Enemy : MonoBehaviour
 {
+    private Player _Player;
+    private Animator animator;
+
+    public EnemyType enemyType;
+
+    private bool isAttack;
+
+    private void Start() {
+        _Player = FindObjectOfType(typeof(Player)) as Player;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if(enemyType==EnemyType.AmangownAttack)
+        {
+            float dist = Vector3.Distance(_Player.transform.position, transform.position);
+            if(dist < 2 && !isAttack)
+            {
+                animator.SetTrigger("attack");
+                isAttack = true;
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Player"))
         {
